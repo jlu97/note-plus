77ec0d14131250f45ffe9180b889e7d206906660
Jayson Isaac <jayson.isaac1@gmail.com>
2020-03-19 21:43:31 -0400

    Load courses on homepage

--- src/Homepage/SignedIn.js
+++ src/Homepage/SignedIn.js
@@ -1,23 +1,67 @@
-import React from 'react';
-import '../App.css';
+import React from "react";
+import "../App.css";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import AuthContext from "../AuthContext";
+import Spinner from "react-bootstrap/Spinner";
 
-function SignedIn(){
-  const favorites = ["Programming Languages"]
-  const favCourses = favorites.map((fav) =>
-    <li key={fav.toString()}>
-      <Link to="/course">
-        {fav}
-      </Link>
-    </li>
-  )
+class SignedIn extends React.Component {
+  static contextType = AuthContext;
 
-  return (
-    <div>
-      <h3>Favorite Courses</h3>
-      <ul>{favCourses}</ul>
-    </div>
-  )
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      favCourses: [],
+      showError: false
+    };
+  }
+
+  componentDidMount() {
+    // axios.get(url, config)
+    axios
+      .get(process.env.REACT_APP_ALL_COURSES_API, {
+        headers: { Authorization: this.context.authToken }
+      })
+      .then(res => {
+        this.setState({
+          loading: false,
+          favCourses: res.data,
+          showError: false
+        });
+      })
+      .catch(err => {
+        this.setState({
+          loading: false,
+          showError: true
+        })
+      });
+  }
+
+  render() {
+    const favCourses = this.state.favCourses.map(fav => (
+      <li key={fav.toString()}>
+        <Link to="/course">{fav}</Link>
+      </li>
+    ));
+
+    if (this.state.loading) {
+      return (
+        <Spinner animation="border" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      );
+    } else if (this.state.showError) {
+      return (<p>Please refresh the page.</p>)
+    } else {
+      return (
+        <div>
+          <h3>Favorite Courses</h3>
+          <ul>{favCourses}</ul>
+        </div>
+      );
+    }
+  }
 }
 
 export default SignedIn;
